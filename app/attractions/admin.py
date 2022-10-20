df05a9dcfbac81f29635e9b3b9eb8507201613fb
horadrim-dev <email.alexss@gmail.com>
2022-10-20 12:16:47 +0700

    mixin fix

--- app/attractions/admin.py
+++ app/attractions/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from .models import *
 from .forms import AttractionForm
+from cms.admin.placeholderadmin import PlaceholderAdminMixin
 
 class PhotoAttractionInline(admin.TabularInline):
     model = Photo
 
 @admin.register(Attraction)
-class ReviewAdmin(admin.ModelAdmin):
+class ReviewAdmin(PlaceholderAdminMixin, admin.ModelAdmin):
     # поле alias будет автоматически заполнено на основе заголовка
     # prepopulated_fields = {
     #     "alias" : ("title",)
--- app/djangocms_reviews/admin.py
+++ app/djangocms_reviews/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from .models import Review
 from .forms import ReviewForm
-from cms.admin.placeholderadmin import PlaceholderAdminMixin
+
 
 @admin.register(Review)
-class ReviewAdmin(PlaceholderAdminMixin, admin.ModelAdmin):
+class ReviewAdmin(admin.ModelAdmin):
     # поле alias будет автоматически заполнено на основе заголовка
     # prepopulated_fields = {
     #     "alias" : ("title",)
