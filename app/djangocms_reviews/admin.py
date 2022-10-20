2ed6ede9ca33768b1da9edbe6b35a0a3fc90267b
horadrim-dev <email.alexss@gmail.com>
2022-10-20 12:13:44 +0700

    adminmixin add

--- app/djangocms_reviews/admin.py
+++ app/djangocms_reviews/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from .models import Review
 from .forms import ReviewForm
-
+from cms.admin.placeholderadmin import PlaceholderAdminMixin
 
 @admin.register(Review)
-class ReviewAdmin(admin.ModelAdmin):
+class ReviewAdmin(PlaceholderAdminMixin, admin.ModelAdmin):
     # поле alias будет автоматически заполнено на основе заголовка
     # prepopulated_fields = {
     #     "alias" : ("title",)
